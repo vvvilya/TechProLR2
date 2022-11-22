d5150fbda1a5bfdbe4ffc992670f0f9e309908b0
NSSeleznjova <nadiaselezneva@yandex.ru>
2022-11-22 22:00:13 +0300

    commit 3

--- main.cpp
+++ main.cpp
@@ -34,3 +34,59 @@ cout «"Acceleration = " « vec[i]->measure() « " g" « endl;
 }
 }
 }
+//////////////////////датчики///////////////////////////////////////////////
+class Acceleration: public dat
+{
+    public:    
+    Acceleration():dat()
+    {
+        measure_unit = "g";//единица измерения g-перегрузка
+        min_measure_range = 0.0;
+        max_measure_range = 500.0;
+    }
+    double measure()
+    {
+       
+        std::random_device rd;
+        std::mt19937 e2(rd());
+        std::uniform_real_distribution<> dist(min_measure_range, max_measure_range);
+        return dist(e2);
+    }
+};
+
+class Gyroscop: public dat
+{
+    public:
+    Gyroscop():dat()
+    {
+        measure_unit = "gradus";
+        min_measure_range = 0;
+        max_measure_range = 90;
+    }
+    double measure()
+    {
+        std::random_device rd;
+        std::mt19937 e2(rd());
+        std::uniform_real_distribution<> dist(min_measure_range, max_measure_range);
+        return dist(e2);
+    }
+
+};
+
+class Position: public dat
+{
+    public:
+    Position():dat()
+    {
+        measure_unit = "km";
+        min_measure_range = 0; 
+        max_measure_range = 1000;
+    }
+    double measure()
+    {
+        std::random_device rd;
+        std::mt19937 e2(rd());
+        std::uniform_real_distribution<> dist(min_measure_range, max_measure_range);
+        return dist(e2);
+    }
+};
